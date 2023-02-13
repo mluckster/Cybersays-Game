e53eeee52035a848d5cbbc07d6a4bf7d5003dfba
mluckster <luckymichael00@gmail.com>
2023-02-12 20:39:20 -0500

    add asynch functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const currLevel = 1;
+let currLevel = 0;
 const lastLevel = 10;
 let compSeq = []
 let playerSeq = []
@@ -26,6 +26,15 @@ function arraysEqual(arr1, arr2) {
     }
 }
 
+async function getInput() {
+    return new Promise((resolve) => {
+        for (let button of boardEl) {
+            button.addEventListener('click', () => {
+                resolve(parseInt(button.id))
+            })
+        }
+    })
+}
 
 
 
@@ -38,32 +47,38 @@ console.log(boardEl)
 console.log(startEl)
 
 //begin.target is the start button
-startEl.addEventListener('click', (begin) => {
+startEl.addEventListener('click', async (begin) => {
     // console.log(begin.target)
+    currLevel = 0;
     compSeq = [];
     playerSeq = [];
-
-
+    
     while (arraysEqual(compSeq, playerSeq)) {
-        for (index = 1; index <= currLevel; index++) {
-            newRand = randInteger()
-            compSeq.push(newRand)
-            console.log('computer sequence is ' + compSeq)
-            // need to render the pattern here  
+        playerSeq = [];
+        currLevel++
+        newRand = randInteger()
+        compSeq.push(newRand)
+        console.log('computer sequence is ' + compSeq)
+        // need to render the pattern here  
+        // boardEl[0] will select individual board buttons
+        
+        // console.log(boardEl)
+        for (index = 0; index < currLevel; index++) {
+            playerSeq.push(await getInput())
+            console.log(playerSeq[index])
+            console.log(compSeq[index])
+            if (playerSeq[index] !== compSeq[index]){
+                break
+            }
         }
-        //this - boardEl[0] will select individual board buttons
-        console.log(boardEl[0])
-        for (let circle of boardEl) {
-            console.log(circle)
-            // console.log(boardEl)
-            circle.addEventListener('click,', (evt2) => {
-                console.log(evt2.target)
-            })
-        }
+        
     }
     console.log('are the arrays equal still? ' + arraysEqual(compSeq, playerSeq))
 
 })
+
+
+
 
 
 
