f4972054205b1854ea847e4eb90686515fa63a80
mluckster <luckymichael00@gmail.com>
2023-02-13 23:50:31 -0500

    took out promises

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let currLevel = 0;
+const winLevel = 2;
 const lastLevel = 10;
 let compSeq = []
 let playerSeq = []
@@ -48,55 +49,35 @@ async function getInput() {
 // maybe try to remove event listener on getInput - right now it's creating an 
 // event listener every time we loop through the rounds of the game
 
-async function toggle (element) {
+async function compToggle (element) {
     return new Promise ((resolve) => {
         setTimeout(() => {
             element.classList.add('active')
-        }, 200)
+        }, 500)
         setTimeout(() => {
             element.classList.remove('active')
             resolve();
-        }, 800)
-    }) 
+        }, 1200)
+    })
 }
 
 // used in dom lab, this only works on event listener - evt.target.classList.add('active')
 // inserting render function here this will be for the computer array render:
 
-async function compRender () {
-    for (indComp=0; indComp<currLevel; indComp++) {
-        await toggle (boardEl[compSeq[indComp]])
-    }
-}
+// async function compRender () {
+//     for (indComp=0; indComp<currLevel; indComp++) {
+//         await compToggle (boardEl[compSeq[indComp]])
+//     }
+// }
+
+
     
-
-
-
-// if (playerSeq.length === 0) {
-//         let i = 0
-//         addActive(i)
-//     }
-// }
-// function addActive (i) {
-//     boardEl[compSeq[i]].classList.add('active')
-//     setTimeout (() => {
-//         removeActive(i)
-//     }, 1000)
-// }
-// function removeActive (i) {
-//     boardEl[compSeq[i]].classList.remove('active')
-//     i++
-//     if (index <= compSeq.length){
-//         addActive ()
-//     }
-//     else {
-//         return
-//     }
-// }
-
-
-
-
+function playToggle (arr) {
+    boardEl[arr[arr.length-1]].classList.add('active')
+    setTimeout(() => {
+        boardEl[arr[arr.length-1]].classList.remove('active')
+    }, 150)
+}
 
 console.log('-----------------')
 // document.getElementById(compSeq[0]).classList.add('active')
@@ -104,54 +85,119 @@ console.log('-----------------')
 
 
 console.log("random integer is" + randInteger())
-
+console.log('board element:')
 console.log(boardEl)
+console.log(boardEl[0])
+console.log(boardEl[2])
+
+
 console.log(startEl)
 
 //begin.target is the start button
-startEl.addEventListener('click', async (begin) => {
-    // console.log(begin.target)
+startEl.addEventListener('click', (begin) => {
     currLevel = 0;
     compSeq = [];
     playerSeq = [];
-
-
-
-
-    
-    while (arraysEqual(compSeq, playerSeq)) {
-        if (currLevel === 10) {
-            console.log(`congrats, you've won the game`)
-        }
-        playerSeq = [];
-        currLevel++
-        newRand = randInteger()
-        compSeq.push(newRand)
-        console.log('computer sequence is ' + compSeq)
-        compRender ()
-        // need to render the pattern here  
-        // boardEl[0] will select individual board buttons
-        
-        // checks every index that the player inputs against the same index on computer
-        // 
-        for (index = 0; index < currLevel; index++) {
-            console.log('-----------------')
-            console.log(playerSeq[index])
-            console.log(compSeq[index])
-            playerSeq.push(await getInput())
-            console.log('-----------------')
-            console.log(playerSeq[index])
-            console.log(compSeq[index])
-            if (playerSeq[index] !== compSeq[index]){
-                // GAME OVER HERE
-                break
-            }
-        }
-
+    // console.log(begin.target)
+    nextRound();
+    for (let button of boardEl) {
+        button.addEventListener('click', gamePlay)
     }
+})
+
+// should maybe try to get away from the wile loop - can call functions instead and use if statement
+
+
+function nextRound () {
+    currLevel++
+    playerSeq = []
+    newRand = randInteger()
+    compSeq.push(newRand)
+    console.log('computer sequence is ' + compSeq)
+    compRender(compSeq)
+}
+
+// THIS FUNCTION NEED TO WORK ON
+function winMessage() {
+    console.log('you have won the game!')
+    document.querySelectorAll('.board').style.backgroundColor = 'blue'
+    const win = document.createElement('div')
+    win.classList.add('win-message')
+    win.textContent = `You've won the Game!`
+}
+
+function gamePlay (evt) {
+    console.log("evt.target")
+    console.log(evt.target)
+    playerSeq.push(parseInt(evt.target.id))
+    playToggle(playerSeq)
+    console.log(playerSeq)
+    if (arraysEqual(compSeq, playerSeq) && currLevel === winLevel) {
+        console.log('you have won the game!')
+        // winMessage() --- need to fix this function and have it display something
+        for (let button of boardEl) {
+            button.removeEventListener ('click', gamePlay)
+        }
+    } else if (arraysEqual(compSeq, playerSeq)) {
+        nextRound()
+    } else if (compSeq[playerSeq.length-1] !== playerSeq[playerSeq.length-1]) {
+        console.log('you failed :)')
+        for (let button of boardEl) {
+            button.removeEventListener ('click', gamePlay)
+        }
+    } else {
+        return
+    }
+}
+
+// function compRender (arr) {
+//     let compIndex = 0
+//     let boardTarget = boardEl[arr[compIndex]]
+//     addActive (boardTarget, compIndex)
+// }
+// function addActive (boardTarget, compIndex) {
+//     setTimeout
+// }
+
+
+// idk whats happening here, not sure how to fix this
+function compRender (arr) {
+    let indComp = 0
+    let element = boardEl[arr[indComp]]
+    setTimeout (() => {
+        element.classList.add('active')
+        setTimeout (() => {
+            element.classList.remove('active')
+            indComp++
+            if (indComp > arr.length) {
+                return
+            } else {
+                compRender (arr)
+            }
+        }, 1200)
+    }, 200)
+}
+
+
+
+
+
+    // while (indComp<arr.length) {
+    //     let element = boardEl[arr[indComp]]
+    //     setTimeout(() => {
+    //         element.classList.add('active')
+    //         setTimeout(() => {
+    //             element.classList.remove('active')
+    //         }, 1300 + indComp*1300)
+    //     }, 200 + indComp*200)
+    // }
+
+
+
+
     console.log('are the arrays equal still? ' + arraysEqual(compSeq, playerSeq))
 
-})
+
 
 
 
